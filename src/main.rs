796ce63ab8e8abe1467db511d7f888565fa6dc92
Caleb <beerscb@gmail.com>
2024-01-02 13:31:36 -0500

    Make the game turn-based

--- src/main.rs
+++ src/main.rs
@@ -6,6 +6,7 @@ mod map;
 mod map_builder;
 mod spawner;
 mod systems;
+mod turn_state;
 
 mod prelude {
     pub use bracket_lib::prelude::*;
@@ -22,6 +23,7 @@ mod prelude {
     pub use crate::map_builder::*;
     pub use crate::spawner::*;
     pub use crate::systems::*;
+    pub use crate::turn_state::*;
 }
 
 use prelude::*;
@@ -29,7 +31,9 @@ use prelude::*;
 struct State {
     ecs: World,
     resources: Resources,
-    systems: Schedule,
+    input_systems: Schedule,
+    player_systems: Schedule,
+    monster_systems: Schedule,
 }
 
 impl State {
@@ -47,13 +51,25 @@ impl State {
             .for_each(|pos| spawn_monster(&mut ecs, &mut rng, pos));
         resources.insert(map_builder.map);
         resources.insert(Camera::new(map_builder.player_start));
+        resources.insert(TurnState::AwaitingInput);
         Self {
             ecs,
             resources,
-            systems: build_scheduler(),
+            input_systems: build_input_scheduler(),
+            player_systems: build_player_scheduler(),
+            monster_systems: build_monster_scheduler(),
         }
     }
 }
+
+// let current_state = self.resources.get::<TurnState>().unwrap().clone();
+// This line needs to be dissected.
+// self.resources.get::<TYPE> requests a resource of a given type.
+// In this case, TurnState.
+// The result is returned as an Option type, which must be .unwrap()'d
+// so that we can access the contents.
+// The final call to clone() duplicates the state. We're now looking at a copy of the
+// current turn state, rather than the original.
 
 impl GameState for State {
     fn tick(&mut self, ctx: &mut BTerm) {
@@ -62,7 +78,19 @@ impl GameState for State {
         ctx.set_active_console(1);
         ctx.cls();
         self.resources.insert(ctx.key);
-        self.systems.execute(&mut self.ecs, &mut self.resources);
+        let current_state = self.resources.get::<TurnState>().unwrap().clone();
+        match current_state {
+            TurnState::AwaitingInput => self
+                .input_systems
+                .execute(&mut self.ecs, &mut self.resources),
+            TurnState::PlayerTurn => {
+                self.player_systems
+                    .execute(&mut self.ecs, &mut self.resources);
+            }
+            TurnState::MonsterTurn => self
+                .monster_systems
+                .execute(&mut self.ecs, &mut self.resources),
+        }
         render_draw_buffer(ctx).expect("Render error");
     }
 }
--- src/systems/mod.rs
+++ src/systems/mod.rs
@@ -1,6 +1,7 @@
 use crate::prelude::*;
 
 mod collisions;
+mod end_turn;
 mod entity_render;
 mod map_render;
 mod player_input;
@@ -12,13 +13,33 @@ mod random_move;
 // are rendered. This is because the render system is applied AFTER the flush.
 // Third, this guarantees that all systems up to this point have finished executing
 // before the next one is run.
-pub fn build_scheduler() -> Schedule {
+pub fn build_input_scheduler() -> Schedule {
     Schedule::builder()
         .add_system(player_input::player_input_system())
+        .flush()
+        .add_system(map_render::map_render_system())
+        .add_system(entity_render::entity_render_system())
+        .build()
+}
+
+pub fn build_player_scheduler() -> Schedule {
+    Schedule::builder()
         .add_system(collisions::collisions_system())
         .flush()
         .add_system(map_render::map_render_system())
         .add_system(entity_render::entity_render_system())
+        .add_system(end_turn::end_turn_system())
+        .build()
+}
+
+pub fn build_monster_scheduler() -> Schedule {
+    Schedule::builder()
         .add_system(random_move::random_move_system())
+        .flush()
+        .add_system(collisions::collisions_system())
+        .flush()
+        .add_system(map_render::map_render_system())
+        .add_system(entity_render::entity_render_system())
+        .add_system(end_turn::end_turn_system())
         .build()
 }
