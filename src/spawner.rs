7984f622564b5211843fb46175d85838b804a8b9
Caleb <beerscb@gmail.com>
2024-01-02 12:05:03 -0500

    Add random move to enemies

--- src/spawner.rs
+++ src/spawner.rs
@@ -1,6 +1,7 @@
 use crate::prelude::*;
 
-// This takes a mutable reference to "world". Notice that this lets you update a World without borrowing that World itself.
+// This takes a mutable reference to "world". Notice that this lets you update a
+// World without borrowing that World itself.
 // This lets us mutate stuff in place without ownership changing hands
 // However, recall that mutable references lock the original data to any other access
 // in the scope where the mutable reference occurs
@@ -12,5 +13,26 @@ pub fn spawn_player(ecs: &mut World, pos: Point) {
             color: ColorPair::new(WHITE, BLACK),
             glyph: to_cp437('@'),
         },
+    ));
+}
+
+// ecs.push is pushing a vector of components (that is, an entity) to the ecs
+// This one has the following components:
+// Enemy (it is an enemy)
+// Point (it has a location, which is "pos" here)
+pub fn spawn_monster(ecs: &mut World, rng: &mut RandomNumberGenerator, pos: Point) {
+    ecs.push((
+        Enemy,
+        pos,
+        Render {
+            color: ColorPair::new(WHITE, BLACK),
+            glyph: match rng.range(0, 4) {
+                0 => to_cp437('E'),
+                1 => to_cp437('O'),
+                2 => to_cp437('o'),
+                _ => to_cp437('g'),
+            },
+        },
+        MovingRandomly {},
     ));
 }
--- src/systems/mod.rs
+++ src/systems/mod.rs
@@ -4,12 +4,21 @@ mod collisions;
 mod entity_render;
 mod map_render;
 mod player_input;
+mod random_move;
 
+// The call to flush() here does a few different things.
+// First, it makes sure all changes up to this point are applied.
+// Second, because those changes are applied, any deleted entities are gone before they
+// are rendered. This is because the render system is applied AFTER the flush.
+// Third, this guarantees that all systems up to this point have finished executing
+// before the next one is run.
 pub fn build_scheduler() -> Schedule {
     Schedule::builder()
         .add_system(player_input::player_input_system())
         .add_system(collisions::collisions_system())
+        .flush()
         .add_system(map_render::map_render_system())
         .add_system(entity_render::entity_render_system())
+        .add_system(random_move::random_move_system())
         .build()
 }
